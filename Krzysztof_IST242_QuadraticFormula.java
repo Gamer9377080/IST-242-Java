ebe01806e8e27cdad98653d955c7be24dfe8604d
Gamer9377080 <kwrzos08@gmail.com>
2026-09-02 11:35:08 -0400

    QuadraticFormula

---
+++ Krzysztof_IST242_QuadraticFormula.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Krzysztof_IST242_QuadraticFormula {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        double p = 3.14;
+        double a, b, c;
+        //gets an imput for a,b,c
+        System.out.print("Enter a real number for a: ");
+        a = scn.nextDouble();
+        System.out.println();
+        System.out.print("Enter a real number for b: ");
+        b = scn.nextDouble();
+        System.out.println();
+        System.out.print("Enter a real number for c: ");
+        c = scn.nextDouble();
+        System.out.println();
+
+        
+
+
+    }
+}
